61c65278d7c03dec54909e9f8273424e1542fe4e
Chee Loong Ng <cheeloongng@Chees-MacBook-Pro.local>
2018-01-26 16:32:15 +0000

    insert scikitlearn

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 '''This is an updated second branch updates '''
 import numpy as np
 import pandas as pd
+from skilearn.modelselection import train_test_split
 
 print('Hello {0}'.format('World'))
 print('Test', 123)
